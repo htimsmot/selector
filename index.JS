c8d9f623fd3c66aa3596e267eb758fe0bfe599ad
htimsmot <132993711+htimsmot@users.noreply.github.com>
2023-06-30 19:26:22 +0100

    Add files via upload

---
+++ index.JS
@@ -0,0 +1,9 @@
+// alert("pow!");
+
+var selected = Math.floor(1 + Math.random() * 7);
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", "./images/image" + selected + ".png");
+
+// "./images/image" + selected + ".png"
